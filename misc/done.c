bced7b31c18adc4ae0da269a9127f61975be32d9
Azrod42 <tom.sorabella@gmail.com>
2022-12-09 23:00:04 +0100

    ongoinyadesbug

---
+++ misc/done.c
@@ -0,0 +1,8 @@
+-Recuperatio des inputs user
+-Recuperation des arg (=) (ex: ARG="test"); //affichage actif dans le main lors de la comande 'exit'
+-Remplacement des $ par la data des arg = //ongoin 
+-Split de l'input user dans char **prompt
+-Redirect //que l'exit de dispo
+-ft_exit //gestion de la sortie du shell sans leaks;
+
++tout les leaks sont gere sur la branch master
--- srcs/pars/pars_replace_equal.c
+++ srcs/pars/pars_replace_equal.c
@@ -6,11 +6,36 @@
 /*   By: tsorabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 17:15:46 by tsorabel          #+#    #+#             */
-/*   Updated: 2022/12/09 18:29:34 by tsorabel         ###   ########.fr       */
+/*   Updated: 2022/12/09 22:58:56 by tsorabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"../../include/minishell.h"
+
+char	*ft_strnstr_len(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!big && len == 0)
+		return (0);
+	if (!little[i])
+		return (((char *)big));
+	while (big[i] && i <= len)
+	{
+		j = 0;
+		if (big[i] == little[j])
+		{
+			while (big[i + j] == little[j] && little[j] && i + j < len)
+					j++;
+			if (!little[j] && len == j)
+				return (&((char *)big)[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
 
 size_t	get_len_replace(t_data *dta)
 {
@@ -21,11 +46,18 @@ size_t	get_len_replace(t_data *dta)
 	j = 0;
 	while (dta->t_prompt[++i + j])
 	{
-		if (dta->t_prompt[i] == '$')
-			while (dta->t_prompt[i + j] != ' ' && dta->t_prompt[i + j])
+		printf("%c", dta->t_prompt[i + j]);
+		if (dta->t_prompt[i + j] == '$')
+		{
+			while (dta->t_prompt[i + j] != ' ' && dta->t_prompt[i + j] != '\0')
+			{
+				printf("%c", dta->t_prompt[i + j]);
 				j++;
+			}
+		}
 	}
-	return (i - j);
+	printf("\n");
+	return (i);
 }
 
 void	replace_arg(t_data *dta)
@@ -38,9 +70,9 @@ void	replace_arg(t_data *dta)
 
 	i = -1;
 	printf("%zu\n", get_len_replace(dta));
-	str = malloc(sizeof(char) * get_len_replace(dta));
 	if (dta->nb_arg != 0)
 	{
+		str = malloc(sizeof(char) * get_len_replace(dta));
 		while (dta->t_prompt[++i])
 		{
 			j = 0;
@@ -48,16 +80,26 @@ void	replace_arg(t_data *dta)
 			{
 				k = -1;
 				l = -1;
-				while (dta->t_prompt[i + j + 1] != ' ')
+				while (dta->t_prompt[i + j + 1] != ' ' && dta->t_prompt[i + j + 1])
 					j++;
+				printf("=%zu\n", j);
 				while (++k < dta->nb_arg)
-					if (ft_strnstr(&dta->t_prompt[i + 1], dta->d_arg[k]->flag, j) != NULL)
-						while (dta->d_arg[k]->data[++l]);
+				{
+					printf("%s\n", ft_strnstr_len(&dta->t_prompt[i + 1], dta->d_arg[k]->flag, j));
+					if (ft_strnstr_len(&dta->t_prompt[i + 1], dta->d_arg[k]->flag, j) != NULL)
+					{
+						printf("find\n");
+						while (dta->d_arg[k]->data[++l])
 							str[i + l] =  dta->d_arg[k]->data[l];
+					}
+				}
 				i += j;
 			}
 			else
 				str[i] = dta->t_prompt[i];
 		}
+		printf("%s\n",str);
+		free(dta->t_prompt);
+		dta->t_prompt = str;
 	}
 }
