94542badde142fe4f0e2706bf41b5a4d9d9c388a
Azrod42 <tom.sorabella@gmail.com>
2022-12-10 15:44:28 +0100

    ok2

--- srcs/redirect.c
+++ srcs/redirect.c
@@ -6,7 +6,7 @@
 /*   By: tsorabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 18:44:54 by tsorabel          #+#    #+#             */
-/*   Updated: 2022/12/10 15:43:13 by tsorabel         ###   ########.fr       */
+/*   Updated: 2022/12/10 15:44:19 by tsorabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	redirect(t_data *dta)
 			dta->exit = 1;
 		else if (ft_strnstr(dta->prompt[0], "clear",
 				ft_strlen(dta->prompt[0])) != NULL)
-			ft_putstr_fd("\e[1;1H\e[2J");
+			ft_putstr_fd("\e[1;1H\e[2J", 1);
 		else
 			printf("%s : commande not found\n", dta->prompt[0]);
 	}
