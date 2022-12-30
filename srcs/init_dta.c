4b5eca169cbc2db35b07d5528aac43b380d2e798
azrod42 <tom.sorabella@gmail.com>
2022-12-30 18:15:23 +0100

    Autopush

--- srcs/init_dta.c
+++ srcs/init_dta.c
@@ -6,7 +6,7 @@
 /*   By: tsorabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 17:33:01 by tsorabel          #+#    #+#             */
-/*   Updated: 2022/12/16 18:07:51 by tsorabel         ###   ########.fr       */
+/*   Updated: 2022/12/30 11:52:08 by tsorabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	init_dta(t_data *dta, char **env, int argc, char **argv)
 	dta->exit = 0;
 	dta->nb_arg = 0;
 	dta->nb_arg_hist = 0;
+	dta->key_done = 0;
 	dta->pipe_str = ft_strdup("init");
 	dta->empty = ft_strdup("E");
 	dta->empty[0] = dta->empty[0] * -1;
